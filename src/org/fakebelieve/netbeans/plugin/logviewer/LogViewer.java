df78620c466789bbfa26d82d14507fd5c6576bc6
Joseph Mocker <mock@fakebelieve.org>
2012-11-19 10:47:48 -0800

    Problem dealing with empty lines.

--- src/org/fakebelieve/netbeans/plugin/logviewer/LogViewer.java
+++ src/org/fakebelieve/netbeans/plugin/logviewer/LogViewer.java
@@ -199,7 +199,7 @@ public class LogViewer implements Runnable {
     }
 
     private void processLine(String line) {
-        while (line.charAt(line.length()-1) == '\r' || line.charAt(line.length()-1) == '\n') {
+        while (line.length() > 0 && (line.charAt(line.length()-1) == '\r' || line.charAt(line.length()-1) == '\n')) {
             line = line.substring(0, line.length() - 1);
         }
 
